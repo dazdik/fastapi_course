3dd8e34c89ea1b88a12b089a95222da9778c3ccd
Дарья Коннова <dashustic@yandex.ru>
2023-09-14 16:44:37 +0300

    finish lesson 3

--- app/main.py
+++ app/main.py
@@ -13,38 +13,23 @@ app = FastAPI()
 async def root():
     return FileResponse('index.html')
 
-
-# новый роут
-@app.get('/custom')
-def read_custom_message():
-    return {'message': 'This is a custom message!'}
-
-
-class SNums(BaseModel):
-    num1: int
-    num2: int
+fake_users = {
+    1: {'username': 'junior_dev', 'email': 'jundev@bk.ru'},
+    2: {'username': 'middle_dev', 'email': 'middledev@bk.ru'},
+}
 
 
-@app.post('/calculate')
-async def calculate(numbers: SNums):
-
-    return JSONResponse({"result": numbers.num1 + numbers.num2})
-
-
-def check_age(age):
-    return age >= 18
+@app.get('/users/{user_id}')
+def get_users(user_id: int):
+    if user_id in fake_users:
+        return fake_users[user_id]
+    return {'error': 'user not found'}
 
 
-@app.post('/user')
-async def create_user(data: User):
-    return {
-        'name': data.name,
-        'age': data.age,
-        'is_adult': check_age(data.age)
-            }
+@app.get('/users/')
+def get_limit_user(limit: int = 10):
+    return dict(list(fake_users.items())[:limit])
 
 
-if __name__ == '__main__':
-    uvicorn.run(app,
-                host='127.0.0.1',
-                port=80)
+if __name__ == "__main__":
+    uvicorn.run("main:app", host='127.0.0.1', port=8066, reload=True, workers=3)
