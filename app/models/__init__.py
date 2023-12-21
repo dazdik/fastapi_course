52ad88dfc2c18b67795301a9d44eb4a2c5f89185
Дарья Коннова <dashustic@yandex.ru>
2023-12-21 16:57:59 +0300

    change structure

--- app/models/__init__.py
+++ app/models/__init__.py
@@ -1,9 +1,6 @@
-__all__ = ("User", "Feedback", "Base", "SchemaFeedBack", "Product", "UserAuth", "ToDo")
+__all__ = ("Base", "Product", "ToDo")
 
-from app.schemas.schemafeedback import SchemaFeedBack
 
 from .base import Base
-from .feedback import Feedback
 from .product import Product
 from .todo import ToDo
-from .user import User, UserAuth
--- app/schemas/schemafeedback.py
+++
@@ -1,6 +0,0 @@
-from pydantic import BaseModel, Field
-
-
-class SchemaFeedBack(BaseModel):
-    name: str = Field(max_length=30)
-    message: str = Field(max_length=150)
